442179214cd53d24f227744074ea5dbd520122a1
CHAPEAU Nicolas <chapenicolas@gmail.com>
2019-03-13 09:51:22 +0100

    first commit

---
+++ LaravelAnswer/app/Http/Controllers/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PageController extends Controller
+{
+    public function about() {
+        return 'About Us Page';
+    }
+
+    public function contact() {
+        return 'Contact Page';
+    }
+
+    public function submitContact() {
+        return 'Submitted Contact';
+    }
+}
+
